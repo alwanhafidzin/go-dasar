664747e72f02a314dde29fa676dffa6e28af0907
alwanhafidzin <alwanhafidzin@gmail.com>
2021-08-11 07:47:46 +0700

    Create perbandingan.go

---
+++ perbandingan.go
@@ -0,0 +1,22 @@
+//membandingkan dua data dengan return boolean
+
+//Operator
+// < > >= <= == !=(Tidak sama dengan)
+package main
+import "fmt"
+
+func main() {
+	var name1 = "Alwan"
+	var name2 = "Alwan"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
