35f67b64d216be4c775b2b285e1ad8519468037f
alwanhafidzin <alwanhafidzin@gmail.com>
2021-08-12 11:35:24 +0700

    Create named.go

---
+++ 24.Named_Return/named.go
@@ -0,0 +1,21 @@
+//biasanya memberi tahu bahwa function mengembalikan value maka mendeklarasikan tipe data return value di function
+//di golang kita bisa membuat variabel secara langsung di tipe data return function nya
+package main
+
+import "fmt"
+
+func main() {
+	a, b, c := getFullName()
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+func getFullName() (firstName string, middleName string, lastName string) {
+	firstName = "Alwan"
+	middleName = "Hafidzin"
+	lastName = "Firdaus"
+	//gak perlu seperi di bawah cukup return saja
+	// return firstName, middleName, lastName
+	return
+}
